3891d60dba667404fc471408436e62270bc82675
jonaraider4@hotmail.com <jonaraider4@hotmail.com>
2023-06-30 06:43:52 -0600

    Servidor y App

---
+++ EmployMe/app/src/main/java/com/example/navbotdialog/EditProfile.java
@@ -0,0 +1,17 @@
+package com.example.navbotdialog;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.ImageButton;
+
+public class EditProfile extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit_profile);
+    }
+
+}
